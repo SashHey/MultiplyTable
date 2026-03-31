29eb806624c0d38e5293fd5569e5d34cb0d0b264
Sasha T Heyburgh <sasha.heyburgh@gmail.com>
2026-03-31 09:52:38 +0200

    Received data sent over to the table activity

--- MultiplicationTables/app/src/main/java/com/example/multiplicationtables/Table.kt
+++ MultiplicationTables/app/src/main/java/com/example/multiplicationtables/Table.kt
@@ -1,6 +1,7 @@
 package com.example.multiplicationtables
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,14 @@ class Table : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_table)
+
+        //receiving data from the main activity
+        val bundle: Bundle? = intent.extras
+        val tableString = bundle?.getString("tableNumber")
+        val tableNumber = tableString?.toInt()
+        //assigned the text view to a variable to display the times tables
+        val multiplyTbl = findViewById<TextView>(R.id.multiplyTableTxt)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
